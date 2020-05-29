4ba77200ec6fb813e44598511b084416006a44d2
Maria Nevarez <mnevarez1220@gmail.com>
2020-05-28 20:24:08 -0600

    added header page

---
+++ Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css'
+
+const HeaderName = () => {
+
+  return (
+    <>
+      <h2 className="header">Maria Nevarez</h2>
+      <h3 className="header2">Full Stack Web Developer</h3>
+    </>
+  )
+}
+
+export default HeaderName;
